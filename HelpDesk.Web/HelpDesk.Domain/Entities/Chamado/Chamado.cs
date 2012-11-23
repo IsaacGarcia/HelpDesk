43977a231d3a0737e081865fa7ba131cb4b22a49
IsaacGarcia <isaac@workker.com.br>
2012-11-22 23:26:44 -0200

    Chamado, SLA, Cliente, Chamados, ChamdoTest

---
+++ HelpDesk.Web/HelpDesk.Domain/Entities/Chamado/Chamado.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HelpDesk.Domain.ComponenteSLA;
+
+namespace HelpDesk.Domain.ComponenteChamado
+{
+    public class Chamado
+    {
+        public virtual string Descricao { get; set; }
+        public virtual DateTime DataDeSolicitacao { get; set; }
+    }
+}
---
+++ HelpDesk.Web/HelpDesk.Domain/Entities/Produto/Produto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HelpDesk.Domain.ComponenteCliente;
+
+namespace HelpDesk.Domain.Entities.ComponenteProduto
+{
+    public class Produto
+    {
+        public virtual SLA SLA { get; set; }
+    }
+}
