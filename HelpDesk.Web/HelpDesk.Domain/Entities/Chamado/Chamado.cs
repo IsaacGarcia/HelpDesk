503810fd5249e743352fc870bbfb9a470614492a
IsaacGarcia <isaac@workker.com.br>
2012-12-25 03:18:41 -0200

    Controle de chamados

--- HelpDesk.Web/HelpDesk.Domain/Entities/Chamado/Chamado.cs
+++ HelpDesk.Web/HelpDesk.Domain/Entities/Chamado/Chamado.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using HelpDesk.Domain.ComponenteSLA;
 
 namespace HelpDesk.Domain.ComponenteChamado
 {
---
+++ HelpDesk.Web/HelpDesk.Web/Models/Cliente.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelpDesk.Web.Models
+{
+    public class Pagina
+    {
+        public virtual IEnumerable<Cliente> Clientes { get; set; }
+        public virtual Cliente Cliente { get; set; }
+        public virtual IDictionary<string,Cliente> ClientesD { get; set; }
+    }
+
+    public class Cliente : IEnumerable<int>
+    {
+        public virtual string Nome { get; set; }
+        public virtual string Endereco { get; set; }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
