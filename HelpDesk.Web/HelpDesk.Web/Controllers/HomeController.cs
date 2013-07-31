455a26e3ec5390411f188f8181215a2b5442ee13
Isaac Garcia <isaac@workker.com.br>
2013-07-31 15:25:31 -0300

    Página principal e Painel de Chamados

---
+++ HelpDesk.Web/HelpDesk.Web/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelpDesk.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Logar()
+        {
+            return Redirect("/Issues");
+        }
+    }
+}
