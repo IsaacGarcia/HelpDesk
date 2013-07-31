323fa8ddf96cde62814a487b364392ee2d294621
Isaac Garcia <isaac@workker.com.br>
2013-07-31 11:32:29 -0300

    Incluindo Bootstrap

--- HelpDesk.Web/HelpDesk.Web/App_Start/BundleConfig.cs
+++ HelpDesk.Web/HelpDesk.Web/App_Start/BundleConfig.cs
@@ -8,6 +8,10 @@ namespace HelpDesk.Web
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap/script").Include(
+                "~/Scripts/bootstrap.min.js",
+                "~/Scripts/bootstrap.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
@@ -24,6 +28,8 @@ namespace HelpDesk.Web
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/Bootstrap/css").Include("~/Content/bootstrap.min.css", "~/Content/bootstrap.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
---
+++ HelpDesk.Web/HelpDesk.Web/Controllers/IssuesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelpDesk.Web.Controllers
+{
+    public class IssuesController : Controller
+    {
+        //
+        // GET: /Issues/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
